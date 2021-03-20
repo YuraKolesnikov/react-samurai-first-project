2a19b4b2061684319f5b003cca03e82d04579871
Yurii Kolesnikov <j.kolesnikovs@team.avguro.ru>
2021-03-20 22:14:42 +0700

    First commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className="Nav">
+      <a className="Nav__link" href="#">Profile</a>
+      <a className="Nav__link" href="#">Messages</a>
+      <a className="Nav__link" href="#">News</a>
+      <a className="Nav__link" href="#">Music</a>
+      <a className="Nav__link" href="#">Settings</a>
+    </nav>
+  )
+}
+
+export default Nav
